9e3028ea8b3ff1978b5dd54643e11cced738a97f
Noor Abdi <57824560+noorfarooqy@users.noreply.github.com>
2022-08-05 22:12:25 +0300

    update all

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/js/app.js',
+                'resources/js/status.js',
             ]
         }),
 
