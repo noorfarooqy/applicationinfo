b3afdb5ef0b704562077439d3b3bd4509ccc7e7d
Noor Abdi <57824560+noorfarooqy@users.noreply.github.com>
2022-08-05 20:31:02 +0300

    update main url as admin

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,18 @@ use Drongotech\Applicationinfo\Controllers\ApplicationController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => config('appinfo.middleware_auth')], function () {
-    Route::prefix('/appinfo')->group(function () {
-        // Route::get('/', function (AppInfoServices $appInfoServices) {
-        //     return 'welcome';
-        // });
-        Route::get('/', [ApplicationController::class, 'appInfo'])->name('appinfo.index');
-        Route::get('/status', [ApplicationController::class, 'appStatus'])->name('appinfo.status');
+    Route::prefix('/admin')->group(function () {
+        Route::prefix('/appinfo')->group(function () {
 
-        Route::prefix('/api')->group(function () {
-            Route::get('/info', [ApplicationController::class, 'apiGetAppInfo'])->name('appinfo.api.update');
-            Route::post('/update', [ApplicationController::class, 'apiUpdate'])->name('appinfo.api.update');
-            Route::post('/logo', [ApplicationController::class, 'apiUpdateLogo'])->name('appinfo.api.update');
+            Route::get('/', [ApplicationController::class, 'appInfo'])->name('appinfo.index');
+            Route::get('/status', [ApplicationController::class, 'appStatus'])->name('appinfo.status');
+
+            Route::prefix('/api')->group(function () {
+                Route::get('/info', [ApplicationController::class, 'apiGetAppInfo'])->name('appinfo.api.update');
+                Route::post('/update', [ApplicationController::class, 'apiUpdate'])->name('appinfo.api.update');
+                Route::post('/logo', [ApplicationController::class, 'apiUpdateLogo'])->name('appinfo.api.update');
+            });
+
         });
 
     });
