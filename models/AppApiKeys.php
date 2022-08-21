d3924a675bd61ab2c975a7e0d62ae32f215eb1c3
Noor Abdi <57824560+noorfarooqy@users.noreply.github.com>
2022-08-21 16:57:45 +0300

    updated on last updated by

--- models/AppApiKeys.php
+++ models/AppApiKeys.php
@@ -1,6 +1,7 @@
 <?php
 namespace Drongotech\Applicationinfo\Models;
 
+use App\Models\User;
 use Drongotech\ResponseParser\Traits\ErrorParser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,6 +12,24 @@ class AppApiKeys extends Model
     use ErrorParser;
     protected $guarded = ['app_id'];
     protected static $failed_to_update_or_create = " Failed to update api keys. Contact admin for assistance ";
+    protected $casts = [
+        'created_at' => 'datetime: Y-m-d H:i:s',
+        'updated_at' => 'datetime: Y-m-d H:i:s',
+    ];
+
+    protected $appends = [
+        'creator', 'updated_by',
+    ];
+
+    public function getCreatorAttribute()
+    {
+        return $this->createdBy?->name;
+    }
+
+    public function getUpdatedByAttribute()
+    {
+        return $this->lastUpdatedBy?->name;
+    }
 
     public function updateOrCreateApikeys($data)
     {
@@ -23,6 +42,15 @@ class AppApiKeys extends Model
         }
     }
 
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function lastUpdatedBy()
+    {
+        return $this->belongsTo(User::class, 'last_updated_by');
+    }
     public function apiKeyProvider()
     {
         return $this->belongsTo(AppApiProviders::class, 'provider_id');
